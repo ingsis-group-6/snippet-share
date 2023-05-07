e75b764c8d49861929fabadc74b6f79807d16fe9
Santiago Encina <70921727+sencina@users.noreply.github.com>
2023-05-07 16:00:43 -0300

    Add files via upload

---
+++ src/main/kotlin/IngSis/snippetshare/SnippetShareApplication.kt
@@ -0,0 +1,11 @@
+package IngSis.snippetshare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetShareApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetShareApplication>(*args)
+}
---
+++ src/test/kotlin/IngSis/snippetshare/SnippetShareApplicationTests.kt
@@ -0,0 +1,13 @@
+package IngSis.snippetshare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetShareApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
