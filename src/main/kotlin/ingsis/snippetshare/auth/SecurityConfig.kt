d7810f8f20cc5a65b8a9c18840995ba91a7e2af4
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2023-06-13 17:39:19 -0300

    funca auth 0 con la 3.0.6

--- src/main/kotlin/ingsis/snippetshare/auth/SecurityConfig.kt
+++ src/main/kotlin/ingsis/snippetshare/auth/SecurityConfig.kt
@@ -2,6 +2,7 @@ package ingsis.snippetshare.auth
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
@@ -10,27 +11,33 @@ import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 
 
-@EnableWebSecurity
+@Configuration
 class SecurityConfig {
     @Value("\${auth0.audience}")
     private val audience: String? = null
 
     @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private val issuer: String? = null
+
     @Bean
-    @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         /*
         This is where we configure the security required for our endpoints and setup our app to serve as
         an OAuth2 Resource Server, using JWT validation.
         */
-        http.authorizeHttpRequests()
-            .requestMatchers("/public").permitAll()
-            .requestMatchers("/private").authenticated()
-            .requestMatchers("/permission").hasAuthority("read:shared-snippets")
-            .and().cors()
-            .and().oauth2ResourceServer().jwt()
-        return http.build()
+        return http.authorizeHttpRequests {
+            it.requestMatchers("/share").permitAll()
+            it.requestMatchers("/share/**").permitAll()
+            it.requestMatchers("/public").permitAll()
+            it.requestMatchers("/private").authenticated()
+            it.requestMatchers("/permission").hasAuthority("SCOPE_read:shared-snippets")
+        }
+            .cors().and()
+            .csrf().disable()
+            .oauth2ResourceServer {
+                it.jwt()
+            }
+            .build()
     }
 
     @Bean
--- src/main/kotlin/ingsis/snippetshare/domains/share/controller/ShareController.kt
+++ src/main/kotlin/ingsis/snippetshare/domains/share/controller/ShareController.kt
@@ -2,11 +2,10 @@ package ingsis.snippetshare.domains.share.controller
 
 import ingsis.snippetshare.domains.share.dto.ShareDTO
 import ingsis.snippetshare.domains.share.model.Share
-import ingsis.snippetshare.domains.share.repository.ShareRepository
 import ingsis.snippetshare.domains.share.service.ShareService
-import ingsis.snippetshare.domains.share.service.ShareServiceImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import java.security.Principal
 import java.util.*
 
 @RestController
@@ -23,25 +22,25 @@ class ShareController {
 
     @PostMapping("/share")
     @ResponseBody
-    fun share(@RequestHeader("Authorization") token: String, @RequestBody shareDto: ShareDTO): Share {
-        val userId = token.split(" ")[1]
+    fun share(principal: Principal, @RequestBody shareDto: ShareDTO): Share {
+        val userId = principal.name
         return shareService.share(shareDto, userId)
     }
 
     @GetMapping("/share")
-    fun getSharedWithMePosts(@RequestHeader("Authorization") token: String): List<Share> {
-        val userId = token.split(" ")[1]
+    fun getSharedWithMePosts(principal: Principal): List<Share> {
+        val userId = principal.name
         return shareService.getSharedPosts(userId)
     }
 
     @GetMapping("/share/shared_with_me")
-    fun getSharedWithMe(@RequestHeader("Authorization") token: String): List<Share> {
-        val userId = token.split(" ")[1]
+    fun getSharedWithMe(principal: Principal): List<Share> {
+        val userId = principal.name
         return shareService.getSharedWithMePosts(userId)
     }
 
     @DeleteMapping("/share/{id}")
-    fun deleteShare(@RequestHeader("Authorization") token: String, @PathVariable(value = "id") id: UUID) {
+    fun deleteShare(principal: Principal, @PathVariable(value = "id") id: UUID) {
         shareService.deleteShare(id)
     }
 
